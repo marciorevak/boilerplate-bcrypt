edfd87dfc35a03b4f9619be11c250d3aac941b6d
marciorevak <80935243+marciorevak@users.noreply.github.com>
2025-08-02 22:33:51 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -11,17 +11,10 @@ const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
-// Corrected the arrow function syntax from "+>" to "=>"
 bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
     console.log(hash);
     
     bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
         console.log(res);
     });
-});
-
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
 });
